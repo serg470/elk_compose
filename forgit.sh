c0d454f766aff99ee5d626866d584b2f23fc6c7b
Sergey Lebedev <serg470@yandex.ru>
2019-10-01 12:11:18 +0300

    Autocommit

--- forgit.sh
+++ forgit.sh
@@ -10,15 +10,17 @@ sed -i '$d' ./logstash/config/logstash.yml
 echo "xpack.monitoring.elasticsearch.password: changeme" >> ./logstash/config/logstash.yml
 sed -i 's/password.*/password => changeme/g' ./logstash/pipeline/logstash.conf
 
+git add .
+git commit -m "Autocommit"
+git push origin master
 
 
-
-#PASSWD=`head -n 1 .env | grep "=" | cut -d'=' -f2`
+PASSWD=`head -n 1 .env | grep "=" | cut -d'=' -f2`
 # Configure Kibana
-#echo "Configure ELK"
-#sed -i '$d' ./kibana/config/kibana.yml
-#echo "elasticsearch.password:" $PASSWD >> ./kibana/config/kibana.yml
+echo "Configure ELK"
+sed -i '$d' ./kibana/config/kibana.yml
+echo "elasticsearch.password:" $PASSWD >> ./kibana/config/kibana.yml
 # Configure Logstash
-#sed -i '$d' ./logstash/config/logstash.yml
-#echo "xpack.monitoring.elasticsearch.password:" $PASSWD >> ./logstash/config/logstash.yml
-#sed -i "s/password.*/password => $PASSWD/g" ./logstash/pipeline/logstash.conf
+sed -i '$d' ./logstash/config/logstash.yml
+echo "xpack.monitoring.elasticsearch.password:" $PASSWD >> ./logstash/config/logstash.yml
+sed -i "s/password.*/password => $PASSWD/g" ./logstash/pipeline/logstash.conf
